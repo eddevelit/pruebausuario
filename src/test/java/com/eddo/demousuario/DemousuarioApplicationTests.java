e565d97a70e814fd018bc54ece89d82484b58c28
Eduardo Ocampo <edd.develit@gmail.com>
2018-05-11 12:46:11 -0500

    Agregando pruebas

--- src/test/java/com/eddo/demousuario/DemousuarioApplicationTests.java
+++ src/test/java/com/eddo/demousuario/DemousuarioApplicationTests.java
@@ -1,9 +1,14 @@
 package com.eddo.demousuario;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.eddo.demousuario.entity.Cuenta;
+import com.eddo.demousuario.entity.Usuario;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -11,6 +16,32 @@ public class DemousuarioApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	}
+	
+	public void returnsNameAndAge ()
+	{
+		Cuenta  c  = new Cuenta();
+		Usuario u  = new Usuario();
+		
+		u.setMail("edd.develit@gmail.com");
+		u.setNombre("Eduardo");
+		c.setNoCuenta(121130248);
+		
+		String n=u.getNombre();
+		String m=u.getMail();
+		int cu = c.getNoCuenta();
+		
+		
+		String nombre = "Eduardo";
+		String mail = "edd.develit@gmail.com";
+		int cuenta = 121130248;
+		
+		assertEquals(nombre,n);
+		assertEquals(mail,m);
+		assertEquals(cuenta,cu);
+		
+		
+		
 	}
 
 }
